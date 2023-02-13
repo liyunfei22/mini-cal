1abcac8df6e1dccfb0cc101df0a399743e486374
李云飞 <liyunfei02@daojia-inc.com>
2023-02-13 19:29:40 +0800

    feat: 添加bar

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,7 +1,24 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/my/index'
   ],
+  tabBar: {
+    custom: true,
+    color: '#000000',
+    selectedColor: '#DC143C',
+    backgroundColor: '#ffffff',
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '首页'
+      },
+      {
+        pagePath: 'pages/my/index',
+        text: '个人中心'
+      }
+    ]
+  },
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
---
+++ src/custom-tab-bar/index.vue
@@ -0,0 +1,80 @@
+<template>
+  <cover-view class="tab-bar">
+    <cover-view class="tab-bar-border"></cover-view>
+      <cover-view v-for="(item, index) in list" :key="index" class="tab-bar-item" @tap="switchTab(index, item.pagePath)">
+        <cover-view :style="{ color: selected === index ? selectedColor : color }">{{item.text}}</cover-view>
+      </cover-view>
+  </cover-view>
+</template>
+
+<script setup>
+import Taro from '@tarojs/taro'
+import { computed }  from 'vue'
+import { useStore } from 'vuex'
+
+const store = useStore()
+const selected = computed(() => store.getters.getSelected)
+
+const color = '#000000'
+const selectedColor = '#DC143C'
+const list = [
+  {
+    pagePath: '/pages/index/index',
+    text: '首页'
+  },
+  {
+    pagePath: '/pages/my/index',
+    text: '个人中心'
+  }
+]
+
+function switchTab(index, url) {
+  setSelected(index)
+  Taro.switchTab({ url })
+}
+
+function setSelected (index) {
+  store.dispatch('setSelected', index)
+}
+</script>
+
+<style lang="scss">
+.tab-bar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100px;
+  background: white;
+  display: flex;
+  padding-bottom: env(safe-area-inset-bottom);
+}
+
+.tab-bar-border {
+  background-color: rgba(0, 0, 0, 0.33);
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 1px;
+  transform: scaleY(0.5);
+}
+
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.tab-bar-item cover-image {
+  width: 54px;
+  height: 54px;
+}
+
+.tab-bar-item cover-view {
+  font-size: 20px;
+}
+</style>
